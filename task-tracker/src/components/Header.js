76df993b93ff59a7a604c1a6675fe8c06009c404
sehan <sehan.22@cse.mrt.ac.lk>
2025-04-16 09:04:56 +0530

    basic

---
+++ task-tracker/src/components/Header.js
@@ -0,0 +1,13 @@
+import Button from './Button'
+
+const Header = () => {
+   return (
+     <header className='header'>
+        <h1>Task Tracker</h1>
+        <Button color='green' text='Add' />
+     </header>
+   )
+ }
+ 
+ export default Header
+ 
